57b553deeeb9a8cd5e10f79d581626ef51c1d20e
unknown <cosminabahna@gmail.com>
2021-10-13 12:13:29 +0300

    gataaaa

--- main.py
+++ main.py
@@ -7,11 +7,12 @@ def citire_lista():
         dimensiune -=1
     return result_list
 
+
 def numar_cifre_prime(n):
     """
     Verifica daca un nr e format doar din cifre prime
     :param n: intreg
-    :return: true daca
+    :return: true daca e doar din cifre prime,false altfel
     """
     while n:
         if (n%10 == 2 or n%10==3 or n%10==5 or n%10==7):
@@ -19,6 +20,11 @@ def numar_cifre_prime(n):
         else:
             return False
     return True
+
+def test_numar_cifre_prime():
+    assert numar_cifre_prime(33) == True
+    assert numar_cifre_prime(11) == False
+
 
 def all_prime(lista):
 
@@ -37,14 +43,54 @@ def get_longest_prime_digits(lista):
              if all_prime(lista[start:end]):
                  lista_secvente.append(lista[start:end])
     max_sec = []
-
     for secventa in lista_secvente:
         if len(secventa)>len(max_sec):
             max_sec = secventa
 
     return max_sec
+
 def test_get_longest_prime_digits():
-    assert get_longest_prime_digits
+    assert get_longest_prime_digits([33,55,355]) == [33, 55,355]
+    assert get_longest_prime_digits([22,33,55,11,66,77,55]) == [22 ,33, 55]
+
+def semne_alternante(i,n):
+    """
+    Verifica daca doua numere au semne alternante
+    :param i: un nr intreg
+    :param n: un nr intreg
+    :return: true - daca doua numere au semne alternante ; false -daca doua numere au semne alternante
+    """
+    if (i<0 and n>0) or (i>0 and n<0):
+        return True
+    return False
+
+
+def all_semne_alternante(lista):
+
+    for i in range(len(lista)-1):
+        if not semne_alternante(lista[i],lista[i+1]):
+            return False
+    return True
+
+def get_longest_alternating_signs(lista):
+
+    lista_secvente = []
+    for start in range (0, len(lista)):
+        for end in range(start+1, len(lista)+1):
+             if all_semne_alternante(lista[start:end]):
+                 lista_secvente.append(lista[start:end])
+    max_sec = []
+    for secventa in lista_secvente:
+        if len(secventa)>len(max_sec):
+            max_sec = secventa
+
+    return max_sec
+
+def test_get_longest_alternating_signs():
+    assert get_longest_alternating_signs([-33,55,-355]) == [-33, 55,-355]
+    assert get_longest_alternating_signs([-22,33,-55,-11,-66,77,55]) == [-22 ,33,-55]
+
+
 def main():
     stop = False
     lista = []
@@ -60,7 +106,12 @@ def main():
             stop = True
         elif command == '1':
             lista = citire_lista()
+        elif command == '2':
+            lista_semne_alt = get_longest_alternating_signs(lista)
+            print(lista_semne_alt)
         elif command == '3':
+            test_numar_cifre_prime()
+            test_get_longest_prime_digits()
             lista_cifreprime = get_longest_prime_digits(lista)
             print(lista_cifreprime)
 
